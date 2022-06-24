8c0c3c7b96ffa8b1cee84caf1b999e62749dccd3
Adedamola Adeyemo <adeyemoadedamola1@gmail.com>
2022-06-24 10:35:30 +0100

    js code

---
+++ index.js
@@ -0,0 +1,7 @@
+let fullName = "Adedamola Adeyemo";
+let height = "5.8 feets";
+let country = "Nigeria";
+
+console.log('Name: ', fullName);
+console.log('Height: ',height);
+console.log('Country: ',country);
